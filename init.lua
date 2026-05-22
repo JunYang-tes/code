a93224b3714b33d58d0e37590084fbff5f205c44
YJ <jun.yang@target-energysolutions.com>
2026-05-22 10:10:23 +0800

    add notification while installing lazy.nvim

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ package.path = vim.g.magic_root .. "/lua/?.lua;" .. vim.g.magic_root .. "/lua/?/
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
+  vim.notify("Installing lazy.nvim...", vim.log.levels.INFO)
   vim.fn.system({
     "git",
     "clone",
